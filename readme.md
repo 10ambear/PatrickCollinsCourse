c6f256466cd87e75952e4cb505c8e782b53cd3fb
Mana <mana@Manas-MBP.lan>
2023-08-07 22:54:20 +0400

    Added deploy script and readme

--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-Test
+This is just a training repo for the Patrick Collins YouTube course. I'm doing this is a refresher.
+
+I'm using foundry for my testing.
+
+Use the following to deploy:
+
+* Run ` source .env ` to add the environment variables to your shell.
+* Run the deployment script:
+  `forge script script/DeploySimpleStorage.s.sol --rpc-url $RPC_URL --broadcast --private-key $TEST_PKEY`
+* To convert from hex to decimal `cast --to-base <hex> dec`
+* Format all the code `forge fmt`
---
+++ script/DeploySimpleStorage.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+// to run the deployment script
+// `source .env`
+// `forge script script/DeploySimpleStorage.s.sol --rpc-url $RPC_URL --broadcast --private-key $TEST_PKEY`
+contract DeploySimpleStorage is Script {
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+        return simpleStorage;
+    }
+}
